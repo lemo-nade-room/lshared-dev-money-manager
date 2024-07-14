06ffed41d99e39d1cab59612654d2dae4a0a1610
tanaka <lemo.nade.engineer.room@gmail.com>
2024-07-14 21:55:48 +0900

    テストケースを作成

--- api/Sources/App/Models/Todo.swift
+++ api/Sources/App/Models/Todo.swift
@@ -4,7 +4,7 @@ import struct Foundation.UUID
 /// Property wrappers interact poorly with `Sendable` checking, causing a warning for the `@ID` property
 /// It is recommended you write your model with sendability checking on and then suppress the warning
 /// afterwards with `@unchecked Sendable`.
-final class Todo: Model, @unchecked Sendable {
+final class Todo: Model, @unchecked Sendable, Hashable {
     static let schema = "todos"
 
     @ID(key: .id)
@@ -25,5 +25,14 @@ final class Todo: Model, @unchecked Sendable {
             id: self.id,
             title: self.$title.value
         )
+    }
+
+    static func == (lhs: Todo, rhs: Todo) -> Bool {
+        lhs.id == rhs.id && lhs.title == rhs.title
+    }
+
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(title)
     }
 }
